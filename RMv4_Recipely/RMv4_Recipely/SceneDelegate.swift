83c9d93752ea08588a03b5265b6f2467fafbc57a
Natalia_Poftalnaya <poftnataly@gmail.com>
2024-02-26 20:53:57 +0300

    Начальная настройка ветки

---
+++ RMv4_Recipely/RMv4_Recipely/SceneDelegate.swift
@@ -0,0 +1,12 @@
+// SceneDelegate.swift
+// Copyright © RoadMap. All rights reserved.
+
+import UIKit
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo _: UISceneSession, options _: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
