bee9a53bc0aa9fb59b938ae2e69872625e1a4535
Anastasiya Omak <ios.developer.by@gmail.com>
2024-03-13 12:44:40 +0200

    Добавлены комментарии к моделе

--- RMv4_Recipely/RMv4_Recipely/RecipesDTO.swift
+++ RMv4_Recipely/RMv4_Recipely/RecipesDTO.swift
@@ -1,19 +1,19 @@
-//
-//  RecipesDTO.swift
-//  RMv4_Recipely
-//
-//  Created by Anastasiya Omak on 12/03/2024.
-//
-
+// RecipesDTO.swift
+// Copyright © RoadMap. All rights reserved.
 
 import Foundation
 
 /// Модель краткого описания рецепта
 struct ShortRecipe {
+    // Имя изображения рецепта
     let imageName: String
+    // название рецепта
     let label: String
+    // Общее время приготовления рецепта в минутах
     let totalTime: Int
+    // Количество калорий
     let calories: Double
+    // URI рецепта
     let uri: String
 
     init(dto: RecipeDTO) {
@@ -27,14 +27,23 @@ struct ShortRecipe {
 
 /// Модель полного описания рецепта
 struct FullRecipe {
+    // Название рецепта
     let label: String
+    // Имя изображения рецепта
     let imageName: String
+    // Общий вес ингредиентов рецепта
     let totalWeight: Double
+    // Общее время приготовления рецепта в минутах
     let totalTime: Int
+    // Общее количество калорий в рецепте
     let totalCalories: Double
+    // Общее количество сахара в рецепте
     let totalSugars: Double
+    // Общее количество белка в рецепте
     let totalProtein: Double
+    // Общее количество жира в рецепте
     let totalFat: Double
+    // Список ингредиентов рецепта
     let ingredients: [String]
 
     init(dto: RecipeDTO) {
@@ -52,16 +61,21 @@ struct FullRecipe {
 
 /// Перечисление для пищевых компонентов
 enum Nutrients {
+    // Калории
     static let enercKcal = "ENERC_KCAL"
+    // Жиры
     static let fat = "FAT"
+    // Сахара
     static let sugars = "CHOCDF"
+    // Белки
     static let proteins = "PROCNT"
 }
 
 /// Структура DTO
 struct HitsDTO: Codable {
+    // Массив объектов HitDTO
     let hits: [HitDTO]
-
+    // Ключ для массива hits в JSON
     enum CodingKeys: String, CodingKey {
         case hits
     }
@@ -71,51 +85,56 @@ struct HitsDTO: Codable {
 
 /// DTO хита
 struct HitDTO: Codable {
+    // Рецепт
     let recipe: RecipeDTO
 }
 
 /// DTO рецепта
 struct RecipeDTO: Codable {
+    // Уникальный идентификатор рецепта
     let uri: String
+    // Название рецепта
     let label: String
+    // Ссылка на изображение рецепта
     let image: String
+    // Дополнительные изображения
     let images: Images
+    // Источник рецепта
     let source: Source
+    // Ссылка на страницу с рецептом
     let url: String
+    // Ссылка для поделиться рецептом
     let shareAs: String
+    // Количество порций
     let yield: Int
-    let dietLabels: [DietLabel]
+    // Метки здоровья
     let healthLabels: [String]
-    let cautions: [Caution]
+    // Список ингредиентов
     let ingredientLines: [String]
+    // Дополнительная информация о ингредиентах
     let ingredients: [Ingredient]
+    // Количество калорий
     let calories, totalCO2Emissions: Double
+    // Класс выбросов CO2
     let co2EmissionsClass: String
+    // Общий вес ингредиентов
     let totalWeight: Double
+    // Общее время приготовления
     let totalTime: Int
+    // Тип кухни
     let cuisineType: [String]
+    // Тип приема пищи
     let mealType: [MealType]
+    // Тип блюда
     let dishType: [DishType]
+    // Общие пищевые компоненты
     let totalNutrients, totalDaily: [String: Total]
+    // Дайджест о рецепте
     let digest: [Digest]
+    // Теги рецепта
     let tags: [String]?
 }
 
-/// Перечисление для предупреждений
-enum Caution: String, Codable {
-    case gluten = "Gluten"
-    case sulfites = "Sulfites"
-    case wheat = "Wheat"
-}
-
-/// Перечисление для диетических меток
-enum DietLabel: String, Codable {
-    case balanced = "Balanced"
-    case highFiber = "High-Fiber"
-    case lowCarb = "Low-Carb"
-    case lowFat = "Low-Fat"
-    case lowSodium = "Low-Sodium"
-}
 
 // MARK: - Digest
 
@@ -133,41 +152,77 @@ struct Digest: Codable {
 
 /// Перечисление для меток
 enum Label: String, Codable {
+    // Кальций
     case calcium = "Calcium"
+    // Углеводы (net)
     case carbohydratesNet = "Carbohydrates (net)"
+    // Углеводы
     case carbs = "Carbs"
+    // Углеводы (net)
     case carbsNet = "Carbs (net)"
+    // Холестерин
     case cholesterol = "Cholesterol"
+    // Энергия
     case energy = "Energy"
+    // Жиры
     case fat = "Fat"
+    // Пищевые волокна
     case fiber = "Fiber"
+    // Фолат эквивалентный (total)
     case folateEquivalentTotal = "Folate equivalent (total)"
+    // Фолат (food)
     case folateFood = "Folate (food)"
+    // Фолиевая кислота
     case folicAcid = "Folic acid"
+    // Железо
     case iron = "Iron"
+    // Магний
     case magnesium = "Magnesium"
+    // Мононенасыщенные жиры
     case monounsaturated = "Monounsaturated"
+    // Ниацин (B3)
     case niacinB3 = "Niacin (B3)"
+    // Фосфор
     case phosphorus = "Phosphorus"
+    // Полиненасыщенные жиры
     case polyunsaturated = "Polyunsaturated"
+    // Калий
     case potassium = "Potassium"
+    // Белки
     case protein = "Protein"
+    // Рибофлавин (B2)
     case riboflavinB2 = "Riboflavin (B2)"
+    // Насыщенные жиры
     case saturated = "Saturated"
+    // Натрий
     case sodium = "Sodium"
+    // Сахарные алкоголи
     case sugarAlcohols = "Sugar alcohols"
+    // Сахар
     case sugars = "Sugars"
+    // Добавленный сахар
     case sugarsAdded = "Sugars, added"
+    // Тиамин (B1)
     case thiaminB1 = "Thiamin (B1)"
+    // Транс-жиры
     case trans = "Trans"
+    // Витамин A
     case vitaminA = "Vitamin A"
+    // Витамин B12
     case vitaminB12 = "Vitamin B12"
+    // Витамин B6
     case vitaminB6 = "Vitamin B6"
+    // Витамин C
     case vitaminC = "Vitamin C"
+    // Витамин D
     case vitaminD = "Vitamin D"
+    // Витамин E
     case vitaminE = "Vitamin E"
+    // Витамин K
     case vitaminK = "Vitamin K"
+    // Вода
     case water = "Water"
+    // Цинк
     case zinc = "Zinc"
 }
 
